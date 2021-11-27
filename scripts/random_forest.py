e94067dd9328d60fa8d6c9f75e452b8fe84ee7d7
JTFouquier <jennietf@gmail.com>
2021-11-26 18:21:13 -0700

    Training/testing R2, visualizers
    pearson train, test, full
    save correlation scatter plots to PDF

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -4,23 +4,20 @@
 import shap
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns
 import numpy as np
-import subprocess
 from matplotlib.backends.backend_pdf import PdfPages
-from scipy import stats
 
 from merf.merf import MERF
-# from merf.viz import plot_merf_training_stats
+from merf.viz import plot_merf_training_stats
+
 from sklearn.inspection import plot_partial_dependence
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
 
 # TODO switch to OneHotEncoder
 from hot_encoding import _dummy_var_creation
 
-# TODO check tutorial for seaborn use
-# sns.set_context("talk")
-
+from scipy.stats.stats import pearsonr
 
 """
 TODO list 
@@ -85,7 +82,6 @@ def setup_df_do_encoding(df_encoded, random_effect, vars_to_encode,
                     [random_effect] + vars_to_encode + [response_var]
     if delta_flag == "raw":
         drop_cols = [random_effect] + vars_to_encode + [response_var]
-        drop_cols = [random_effect] + vars_to_encode + [response_var]
 
     df_encoded_cols_dropped = df_encoded.drop(drop_cols, axis=1)
     # TODO remove columns that have "ref"
@@ -94,22 +90,52 @@ def setup_df_do_encoding(df_encoded, random_effect, vars_to_encode,
     df_encoded_cols_dropped.to_csv(out_file_prefix +
                                    "-dummy-variables-dropped-columns.txt",
                                    index=False, sep="\t")
-    # final variables for RF or MERF
-    # feature_list = [ x for x in feature_list if "Timepoint" not in x ]
-    # feature_list = [ x for x in feature_list if "FertilizerCost" not in x ]
-    feature_list = [ x for x in feature_list if "_reference" not in x ]
-    # feature_list = [ x for x in feature_list if "Timepoint" not in x ]
 
-    x = df_encoded[feature_list]
-    # print(feature_list)
+    feature_list = [x for x in feature_list if "_reference" not in x]
+
+    x_for_split = df_encoded[feature_list + ["StudyID"]]
+    x = df_encoded[feature_list]  # StudyID cannot be in features
     if re_timepoint == "re_timepoint":
-        z = df_encoded[["Timepoint_reference"]]
+        # z = df_encoded[["Timepoint_reference"]]
+        z = np.ones((len(x), 1))
     if re_timepoint == "no_re":
         z = np.ones((len(x), 1))
-    clusters_train = df_encoded["StudyID"]
+    clusters = df_encoded["StudyID"]
     y = df_encoded[response_var]
 
-    return x, z, clusters_train, y, feature_list, dummy_dict
+    # Split into training and testing to check quality of model
+    x_train, x_test, y_train, y_test = train_test_split(x_for_split, y, test_size=0.2,
+                                                        random_state=42)
+    print("********TRAINING USING MERF")
+    # clusters for each individual
+    clusters_train = x_train["StudyID"]
+    clusters_test = x_test["StudyID"]
+
+    # remove clusters from features X
+    x_train = x_train[feature_list]
+    x_test = x_test[feature_list]
+
+    z_train = np.ones((len(x_train), 1))
+    z_test = np.ones((len(x_test), 1))
+
+    mrf_training, train_test_plots = train_merf_model(x_train, z_train,
+                                                      clusters_train, y_train)
+
+    y_train_estimate = mrf_training.predict(x_train, z_train, clusters_train)
+    y_test_estimate = mrf_training.predict(x_test, z_test, clusters_test)
+
+    corr_pearson_train = pearsonr(y_train_estimate, y_train)
+    print("R2 training", corr_pearson_train)
+
+    corr_pearson_test = pearsonr(y_test_estimate, y_test)
+    print("R2 testing", corr_pearson_test)
+
+    log_string = "Training R2: " + str(corr_pearson_train) + \
+                 "\nTesting R2: " + str(corr_pearson_test)
+
+    corr_plot_test = scatter_plot(y_test_estimate, y_test)
+
+    return x, z, clusters, y, feature_list, dummy_dict, log_string, corr_plot_test
 
 
 def merf_training_stats_plot(mrf, num_clusters):
@@ -120,20 +146,22 @@ def merf_training_stats_plot(mrf, num_clusters):
     return training_stats_plot
 
 
-def y_estimate_y_known_correlation(y_predicted, y):
-    corr = stats.pearsonr(y_predicted, y)
-    log_string = "Predicted vs observed response R2: " + \
-                 str(round((corr[0] * corr[0]), 3)) + "p-value: " + \
-                 str(round(corr[1], 3))
-    return log_string
+def scatter_plot(y_axis, x_axis):
+    x_axis = np.array(x_axis)
+    y_axis = np.array(y_axis)
+    plt.scatter(y_axis, x_axis)
+    corr_plot = plt.gcf()
+    plt.close()
+
+    return corr_plot
 
 
 def train_merf_model(x, z, clusters, y):
     # TODO CHANGE THIS later! NEEDS MORE ITERATIONS
     mrf = MERF(max_iterations=max_iters)
     mrf.fit(x, z, clusters, y)
-    training_stats_plot = merf_training_stats_plot(mrf, 7)
-
+    training_stats_plot = plot_merf_training_stats(mrf, 7)
+    plt.close()
     return mrf, training_stats_plot
 
 
@@ -170,9 +198,6 @@ def important_shapley_features(shap_values, x):
 
 
 def shap_explainer(trained_forest_model, x):
-    print("shap explainer")
-    print(x.columns)
-    print(x.head())
     explainer = shap.TreeExplainer(trained_forest_model)
     shap_values = explainer.shap_values(x)
 
@@ -236,7 +261,7 @@ def main(in_file, out_file, random_forest_type, random_effect, sample_ID,
     to_drop = [random_effect, sample_ID]
     encode = [i for i in categoric_columns if i not in to_drop]
 
-    x, z, clusters, y, feature_list, dummy_dict = \
+    x, z, clusters, y, feature_list, dummy_dict, log_string, corr_plot_test = \
         setup_df_do_encoding(df_encoded=df, random_effect=random_effect,
                              vars_to_encode=encode, response_var=response_var,
                              delta_flag=delta_flag, join_flag=join_flag)
@@ -244,22 +269,28 @@ def main(in_file, out_file, random_forest_type, random_effect, sample_ID,
     if random_forest_type == "mixed":
         mrf, training_stats_plot = train_merf_model(x, z, clusters, y)
         y_predicted = mrf.predict(x, z, clusters)
-        mrf = mrf.trained_rf  # TODO why is this trained fixed effects
-        # mrf = mrf.trained_fe_model # TODO VER. is old or new in MERF tool?
+
+        rf = mrf.trained_fe_model
+
+        pearson_r = pearsonr(y_predicted, y)
+        print("pearson correlation y_predicted, y known full data")
+        print(pearson_r)
+        log_string += "\nR2 Full: " + str(pearson_r)
     else:
         mrf, training_stats_plot = train_rf_model(x, y)
         y_predicted = mrf.predict(x)
 
-    log_info = y_estimate_y_known_correlation(y_predicted, y)
+    corr_plot = scatter_plot(y_predicted, y)
 
     # .pdf is final snakemake check, but rename files with prefix
     out_file_prefix = out_file.split(".pdf")[0]
 
     f_out = open(out_file_prefix + "-log.txt", "w")
-    f_out.write(log_info)
+    f_out.write(log_string)
     f_out.close()
 
-    plot_list, feature_importance, top_features_list = shap_explainer(mrf, x)
+    # TODO this only uses the fixed effects RF
+    plot_list, feature_importance, top_features_list = shap_explainer(rf, x)
     feature_importance.to_csv(out_file_prefix + "-feature-importance.txt",
                               sep='\t', mode='a')
 
@@ -276,10 +307,13 @@ def main(in_file, out_file, random_forest_type, random_effect, sample_ID,
     df = pd.DataFrame({'important.features': top_features_list,
                        'decoded.features': decoded_top_features_list})
     df.to_csv(out_file_prefix + "-top-features.txt", index=False, sep="\t")
-    plot_partial_dependence(mrf, x, features=top_features_list)
+    plot_partial_dependence(rf, x, features=top_features_list)
     plot_some_partial_dependence = plt.gcf()
     plot_some_partial_dependence.set_size_inches(width, height)
     plot_list.append(plot_some_partial_dependence)
+    plot_list.append(training_stats_plot)
+    plot_list.append(corr_plot_test)
+    plot_list.append(corr_plot)
     build_result_pdf(out_file, plot_list)
 
 
