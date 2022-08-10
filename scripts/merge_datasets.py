a6e18628b0da0acac04dddfb10cbb7cdaec774c1
JTFouquier <jennietf@gmail.com>
2022-08-10 12:06:45 -0600

    merge dataset update to use json file
    This allows merging after dim reduction; streamline

--- scripts/merge_datasets.py
+++ scripts/merge_datasets.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from functools import reduce
+import json
 
 # From config first, then from rule
 output_folder = snakemake.config["out"] + snakemake.config["path_merged_data"]
@@ -13,13 +14,47 @@ dataset_file_list = snakemake.input["dataset_list"]
 # TODO metadata-pcas needs be clear as this can be done on other datasets
 # TODO add name of dataset to folder and file names to stay organized
 
+dataset_json = {
+  "datasets": {
+    "dataset": [
+      {
+        "ds_name": "DS_ASVS",
+        "dim_method": "SCNIC",
+        "file_path": "random-forest/TEST/HDL/01-DIM-SCNIC-biom/SCNIC_modules_for_workflow.txt",
+        "param_dict": ""
+      },
+      {
+        "ds_name": "DS_metadata",
+        "dim_method": "PCA",
+        "file_path": "data/hdl-test/real-data-no-asvs.txt",
+        "param_dict": ""
+      }
+    ]
+  }
+}
+
+dataset_json = json.dumps(dataset_json)
+dataset_json = json.loads(dataset_json)
+
 df_list = []
 df_dict = {}
-for i in range(len(dataset_file_list)):
-    df = pd.read_csv(dataset_file_list[i], sep="\t", index_col=sample_id)
-    df_name = 'df' + str(i)
-    df_dict[df_name] = df
-    df_list.append(df_dict[df_name])
+
+unique_cols = []
+for dataset in dataset_json:
+    for k, v in dataset_json[dataset].items():
+        for i in v:
+            df = pd.read_csv(i["file_path"], sep="\t",
+                             index_col=sample_id)
+            df_dict[i["ds_name"]] = df
+            unique_cols += list(df.columns)
+            df_list.append(df)
+
+# TODO
+
+if len(set(unique_cols)) == len(unique_cols):
+    pass
+else:
+   print("Warning: Dataset columns are not unique and may create problems")
 
 # TODO VERIFY THIS MERGE *** review merge decisions ***
 df = reduce(lambda x, y: pd.merge(x, y, left_index=True, right_index=True,
@@ -27,6 +62,44 @@ df = reduce(lambda x, y: pd.merge(x, y, left_index=True, right_index=True,
                                   sort=False), df_list)
 #
 # df = df[df.columns.drop(list(df.filter(regex='_reference')))]
+print(df)
+
+def _subset_simple(df_input, drop_rows, subset_rows, drop_cols, constrain_cols):
+
+    for k, v in drop_rows.items():
+        try:
+            df_input = df_input.loc[(df_input[k] != v)]
+        except:
+            pass
+
+    for k, v in subset_rows.items():
+        try:
+            df_input = df_input.loc[(df[k] == v)]
+        except:
+            pass
+
+    if constrain_cols and drop_cols:
+        print("Cannot have features (arguments) in 'drop_cols' and "
+              "'constrain_cols' parameters")
+        return
+
+    if constrain_cols:
+        df_input = df_input[constrain_cols]
+    else:
+        pass
+
+    df_input = df_input.drop(drop_cols, axis=1)
+
+    return df_input
+
+
+drop_rows = {"SexualClassification": "Women"}
+constrain_rows = {"Diet": "Agrarian"}
+drop_cols = ["Inflammation", "TotalCholesterol", "PCA", "HOMAIR"]
+constrain_cols = []
+
+df = _subset_simple(df, drop_rows, constrain_rows, drop_cols,
+                    constrain_cols)
 
 df.to_csv(output_folder + "final-merged-dfs.txt", sep="\t")
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,35 +2,6 @@
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-def _subset_simple(df, drop_rows, subset_rows, drop_cols, subset_cols):
-
-    for k, v in drop_rows.items():
-        try:
-            df = df.loc[(df[k] != v)]
-        except:
-            pass
-
-    for k, v in subset_rows.items():
-        try:
-            df = df.loc[(df[k] == v)]
-        except:
-            pass
-
-    # Can't have both
-    if subset_cols:
-        if drop_cols:
-            print("Cannot have features (arguments) in 'drop_cols' and "
-                  "'subset_cols' parameters")
-
-    if subset_cols:
-        df = df[subset_cols]
-    else:
-        pass
-
-    df = df.drop(drop_cols, axis=1)
-
-    return df
-
 def _build_result_pdf(out_file, out_file_prefix, plot_list,
                       plot_file_name_list):
     pp = PdfPages(out_file)
