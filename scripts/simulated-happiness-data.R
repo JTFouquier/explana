e04757d0a78e5fd5c34af1dd02985038e79c38e7
JTFouquier <jennietf@gmail.com>
2023-06-11 23:34:15 -0600

    update simulated dataset

--- scripts/simulated-happiness-data.R
+++ scripts/simulated-happiness-data.R
@@ -10,6 +10,47 @@ library("faux")
 out_file <- "data/simulation/simulation.txt"
 
 
+# vars
+
+# two cohorts (A and)
+subjects_per_cohort <- 40
+happiness_start <- 60
+
+std_dev_random_intercepts <- 7
+total_timepoints <- 5
+
+# for initializing dataframe
+c_zeros <- rep(0, total_timepoints)
+c_char <- rep("", total_timepoints)
+
+green_blue_effect <- -15
+
+cohort_a_effect <- -9
+cohort_b_effect <- -4
+
+income_high_effect <- 5
+income_low_effect <- -5
+
+relationship_s_effect <- -4
+relationship_p_effect <- 4
+relationship_s_p_effect <- 6
+relationship_p_s_effect <- -10
+
+# Explanation of variables
+
+# microbe_b (numerical)
+# microbe b - starts low but levels out after first timepoint
+# in all individuals
+
+# microbe_a (numerical)
+# increases in therapy_1, stays the same therapy_2
+
+# medication (categorical)
+# all pills have an equal effect on happiness; interaction Green_Blue causes negative impact on happiness; blue is not found at timepoint 1 to show that this interaction is unable to be found using the First delta dataset. Change is at end of study. 
+
+# relationship (categorical)
+# shows important in first deltas (change is at start)
+
 my_sample <- function(x, plus_minus) {
   # get a random number plus or minus input value
   my_min <- x - plus_minus
@@ -36,7 +77,6 @@ base_dataframe <- function(df, subjects_list, subjects, cohort) {
   subjects_half1 <- subjects[seq(1, length(subjects), 2)]
   subjects_half2 <- subjects[!(subjects %in% subjects_half1)]
 
-
   df <- df %>%
     mutate(therapy = case_when(
       study_id %in% subjects_half1 ~ "therapy_1",
@@ -51,7 +91,7 @@ get_random_intercepts <- function(df, study_id_modifier) {
   total_subjects <- length(unique(df$study_id))
 
   # random intercepts
-  subject_sd <- 3 # SD for the subjects' random intercept
+  subject_sd <- std_dev_random_intercepts
 
   sub <- tibble(
     study_id = paste0(1:total_subjects, study_id_modifier),
@@ -64,44 +104,17 @@ get_random_intercepts <- function(df, study_id_modifier) {
   return(df)
 }
 
-
-# random slopes
-# subject_version_sd <- 20
-# subject_i_version_cor <- -0.2
-# total_subjects <- 10
-
-# sub <- faux::rnorm_multi(
-#   n = total_subjects,
-#   vars = 2,
-#   r = subject_i_version_cor,
-#   mu = 0, # means of random intercepts and slopes are always 0
-#   sd = c(subject_sd, subject_version_sd),
-#   varnames = c("sub_i", "sub_version_slope")
-# ) %>%
-#   mutate(
-#     study_id = 1:total_subjects,
-#     therapy = rep(c("therapy_1", "therapy_2"),
-#     each = total_subjects / 2) # between-subjects factor
-#   )
-
-# cat_placeholder <- c("", "", "", "", "")
-# num_placeholder <- c(0, 0, 0, 0, 0)
-
-c_zeros <- rep(0, 5)
-c_char <- rep("", 5)
-
 # Some are placeholders here, but modified later
 simulated_df_for_workflow <- function(
   happiness_original = c_zeros,
   cohort = c_char, cohort_effect = c_zeros,
   timepoint = c(1, 2, 3, 4, 5), study_id = c_char,
-  microbe_a = c_zeros, microbe_b = c_zeros, microbe_c = c_zeros,
-  microbe_a_effect = c_zeros,
-  microbe_b_effect = c_zeros,
-  microbe_c_effect = c_zeros,
-  bonus = rep("no", 5), bonus_effect = c_zeros,
+  microbe_a = c_zeros, microbe_b = c_zeros,
+  microbe_a_effect = c_zeros, microbe_b_effect = c_zeros,
+  relationship = c_char, relationship_effect = c_zeros,
   therapy = c_char, therapy_effect = c_zeros,
-  medicine = c_char, medicine_effect = c_zeros,
+  sun = c_char, medicine = c_char,
+  med_green_blue_effect = c_zeros,
   income = c_char, income_effect = c_zeros,
   happiness = c_zeros, total_subjects, study_id_modifier
   ) {
@@ -111,12 +124,11 @@ simulated_df_for_workflow <- function(
   study_id <- c(study_id, study_id, study_id, study_id, study_id)
   df <- data.frame(happiness_original, timepoint,
                    cohort, cohort_effect, study_id,
-                   microbe_a, microbe_b, microbe_c,
-                   microbe_a_effect, microbe_b_effect,
-                   microbe_c_effect, bonus, bonus_effect,
+                   microbe_a, microbe_b, microbe_a_effect,
+                   microbe_b_effect, relationship, relationship_effect,
                    therapy, therapy_effect, medicine,
-                   medicine_effect, income,
-                   income_effect, happiness)
+                   med_green_blue_effect, sun,
+                   income, income_effect, happiness)
 
   num_columns <- length(colnames(df))
 
@@ -138,63 +150,34 @@ simulated_df_for_workflow <- function(
   df_cohort_b <- base_dataframe(df, cohort_b_subjects, subjects, "B")
   df <- rbind(df_cohort_a, df_cohort_b)
 
-  # get half of subjects to add variety
-  r <- list_piece(subjects, 2)
-  subjects_half1 <- r[[1]]
-  subjects_half2 <- r[[2]]
-
-  df <- df %>%
-    mutate(income =
-    case_when(study_id %in% subjects_half1 ~ "high",
-              study_id %in% subjects_half2 ~ "low"))
-
-  # Add some no effects to both therapys only if their lifestyle is unhealthy
-  # Demonstrates more categorical vars
+  # add income and sun features
   df <- df %>%
     group_by(study_id) %>%
-    mutate(bonus = case_when(
-
-        # Time 1, 2, 3
-        income == "low" & timepoint == 1 ~ "yes",
-        income == "low" & timepoint == 2 ~ "yes",
-        income == "low" & timepoint == 3 ~ "yes",
-
-        # Time 4
-        income == "low" & timepoint == 4
-        & cohort == "A" ~ "no",
-        income == "low" & timepoint == 4
-        & cohort == "B" ~ "yes",
-
-        # Time 5
-        income == "low" & timepoint == 5
-        & cohort == "B" ~ "no",
-        income == "low" & timepoint == 5
-        & cohort == "A" ~ "yes",
-
-        income == "high" ~ "yes")) %>%
-
-    mutate(microbe_a = case_when(
-      therapy == "therapy_1" & timepoint == 1 ~ my_sample(0.2, 0.03),
-      therapy == "therapy_1" & timepoint == 2 ~ my_sample(0.2, 0.03),
-      therapy == "therapy_1" & timepoint == 3 ~ my_sample(0.3, 0.03),
-      therapy == "therapy_1" & timepoint == 4 ~ my_sample(0.3, 0.03),
-      therapy == "therapy_1" & timepoint == 5 ~ my_sample(0.4, 0.03),
-      therapy == "therapy_2" ~ 0.2)) %>%
-
-    mutate(microbe_b = case_when(
-      (therapy == "therapy_2" & timepoint == 1) ~ 0.1,
-      (therapy == "therapy_2" & timepoint == 2) ~ 0.4,
-      (therapy == "therapy_2" & timepoint == 3) ~ 0.4,
-      (therapy == "therapy_2" & timepoint == 4) ~ 0.4,
-      (therapy == "therapy_2" & timepoint == 5) ~ 0.4,
-      therapy == "therapy_1" ~ 0.1)) %>%
+    mutate(income = sample(c("high", "low"), 1),
+           sun = sample(c("yes", "no"), 1)) %>%
     ungroup()
 
+  # add relationship and microbe_b features
+  df <- df %>%
+    group_by(study_id) %>%
+    rowwise() %>%
+    mutate(relationship = case_when(
+           timepoint %in% 3:5 ~ "p",
+           timepoint %in% 1:2 ~ sample(c("p", "p", "p", "s"), 1))
+      ) %>%
+    mutate(microbe_b = case_when(
+           timepoint == 1 ~ 0.1,
+           timepoint %in% 2:5 ~ 0.5)) %>%
+    ungroup()
+
+  # split into thirds; more are affected & in group2
+  # for both unaffected and affected
   r <- list_piece(subjects, 3)
+
   med_unaffected <- r[[1]]
   med_affected <- r[[2]]
 
-  r <- list_piece(med_affected, 2)
+  r <- list_piece(med_affected, 3)
   med_affected_1 <- r[[1]]
   med_affected_2 <- r[[2]]
 
@@ -202,82 +185,131 @@ simulated_df_for_workflow <- function(
   med_unaffected_1 <- r[[1]]
   med_unaffected_2 <- r[[2]]
 
-
-  # all pills have an equal effect on happiness; interaction red_green
+  # Medication
   df <- df %>%
-  mutate(medicine = case_when(
-      study_id %in% med_affected_1 & timepoint %in% 1:2 ~ "GreenPill",
-      study_id %in% med_affected_1 & timepoint %in% 3:5 ~ "RedPill",
+    group_by(study_id) %>%
+    rowwise() %>%
+    mutate(medicine = case_when(
+      timepoint == 1 ~ sample(c("Pink", "Purple", "Red"), 1),
 
-      study_id %in% med_affected_2 & timepoint %in% 1:2 ~ "RedPill",
-      study_id %in% med_affected_2 & timepoint %in% 3:5 ~ "GreenPill",
+      # affected individuals (interaction effects)
+      study_id %in% med_affected_1 & timepoint %in% 2:3 ~ "Blue",
+      study_id %in% med_affected_1 & timepoint %in% 4:5 ~ "Green",
 
-      # TODO if keeping same pill, change time
-      study_id %in% med_unaffected_1 & timepoint %in% 1:2 ~ "RedPill",
-      study_id %in% med_unaffected_1 & timepoint %in% 3:5 ~ "RedPill",
+      # THIS IS THE INTERACTION TERM. Only effects happen here (green_blue)
+      study_id %in% med_affected_2 & timepoint %in% 2:3 ~ "Green",
+      study_id %in% med_affected_2 & timepoint %in% 4:5 ~ "Blue",
 
-      study_id %in% med_unaffected_2 & timepoint %in% 1:2 ~ "GreenPill",
-      study_id %in% med_unaffected_2 & timepoint %in% 3:5 ~ "GreenPill",
+      # unaffected individuals (no interaction)
+      study_id %in% med_unaffected_1 & timepoint %in% 2:3 ~ "Blue",
+      study_id %in% med_unaffected_1 & timepoint %in% 4:5 ~ "Blue",
+
+      study_id %in% med_unaffected_2 & timepoint %in% 2:3 ~ "Green",
+      study_id %in% med_unaffected_2 & timepoint %in% 4:5 ~ "Green"
     ))
 
   add_effects <- function(df, subjects) {
 
     for (i in subjects) {
-      t1_to_t2 <- subset(df, study_id == i & timepoint %in% 1:2)
-      t3_t5 <- subset(df, study_id == i & timepoint %in% 3:5)
+      t2_t3 <- subset(df, study_id == i & timepoint %in% 2:3)
+      t4_t5 <- subset(df, study_id == i & timepoint %in% 4:5)
+      t1 <- subset(df, study_id == i & timepoint == 1)
+      t2 <- subset(df, study_id == i & timepoint == 2)
+      t3 <- subset(df, study_id == i & timepoint == 3)
       t4 <- subset(df, study_id == i & timepoint == 4)
       t5 <- subset(df, study_id == i & timepoint == 5)
 
-      if ("RedPill" %in% t1_to_t2$medicine
-      && "GreenPill" %in% t3_t5$medicine) {
-        df$medicine_effect[df$timepoint %in% 3:5
-        & df$study_id == i] <- my_sample(-8, 1)
+      # medication med_green_blue_effect
+      if ("Green" %in% t2_t3$medicine
+      && "Blue" %in% t4_t5$medicine) {
+        df$med_green_blue_effect[df$timepoint %in% 4:5
+        & df$study_id == i] <- my_sample(green_blue_effect, 2)
       }
 
-      df$bonus_effect[df$study_id == i & df$bonus == "yes"] <- my_sample(3, 1)
+      # relationship partnered effect
+      df$relationship_effect[df$study_id == i
+      & df$relationship == "p"] <- my_sample(relationship_p_effect, 1)
 
-      if ("no" %in% t4$bonus && "yes" %in% t5$bonus) {
-        df$bonus_effect[df$timepoint == 5
-        & df$study_id == i] <- my_sample(7, 1)
+      # relationship single effect
+      df$relationship_effect[df$study_id == i
+      & df$relationship == "s"] <- my_sample(relationship_s_effect, 1)
+
+      # time 1_2 change in relationship
+      if ("s" %in% t1$relationship && "p" %in% t2$relationship) {
+        df$relationship_effect[df$timepoint == 2
+        & df$study_id == i] <- my_sample(relationship_s_p_effect, 2)
+      }
+      if ("p" %in% t1$relationship && "s" %in% t2$relationship) {
+        df$relationship_effect[df$timepoint == 2
+        & df$study_id == i] <- my_sample(relationship_p_s_effect, 2)
       }
 
-      if ("yes" %in% t4$bonus && "no" %in% t5$bonus) {
-        df$bonus_effect[df$timepoint == 5
-        & df$study_id == i] <- my_sample(-4, 1)
+      # time 1_3 change in relationship
+      if ("s" %in% t2$relationship && "p" %in% t3$relationship) {
+        df$relationship_effect[df$timepoint == 3
+        & df$study_id == i] <- my_sample(relationship_s_p_effect, 2)
       }
-
-      # if ("yes" %in% t4$bonus && "yes" %in% t5$bonus) {
-      #   df$bonus_effect[df$timepoint == 5
-      #   & df$study_id == i] <- my_sample(4, 1)
-      # }
+      if ("p" %in% t2$relationship && "s" %in% t3$relationship) {
+        df$relationship_effect[df$timepoint == 3
+        & df$study_id == i] <- my_sample(relationship_p_s_effect, 2)
+      }
     }
 
-  df <- df %>%
+    # ADD EFFECTS for:
+    # cohort, microbe_b, income
+    df <- df %>%
+      group_by(study_id) %>%
+      group_by(timepoint) %>%
+      rowwise() %>%
+      mutate(cohort_effect = case_when(
+             cohort == "A" ~ cohort_a_effect,
+             cohort == "B" ~ cohort_b_effect),
+
+             microbe_b_effect = microbe_b,
+
+             income_effect = case_when(
+             income == "high" ~ income_high_effect,
+             income == "low" ~ income_low_effect)) %>%
+      ungroup()
+
+  # effects for microbe_a different in therapy
+  # them merge dfs
+  df_i1 <- df %>%
+    filter(sun == "yes") %>%
     group_by(study_id) %>%
     rowwise() %>%
-    mutate(cohort_effect = case_when(
-           cohort == "A" ~ my_sample(-6, 1),
-           cohort == "B" ~ my_sample(6, 1)),
-
-           microbe_b_effect = microbe_b * 4,
-
-           income_effect = case_when(
-           income == "high" ~ my_sample(3, 1),
-           income == "low" ~ my_sample(-3, 1)),
-
-           microbe_a_effect = microbe_a * 2) %>%
+    mutate(microbe_a_effect = case_when(
+           timepoint == 1 ~ my_sample(10, 3),
+           timepoint == 2 ~ my_sample(11, 3),
+           timepoint == 3 ~ my_sample(20, 3),
+           timepoint == 4 ~ my_sample(22, 3),
+           timepoint == 5 ~ my_sample(21, 3))) %>%
+    mutate(microbe_a_effect = 0.01 * microbe_a_effect,
+           microbe_a = microbe_a_effect) %>%
     ungroup()
+
+  df_i2 <- df %>%
+    filter(sun == "no") %>%
+    group_by(study_id) %>%
+    rowwise() %>%
+    mutate(microbe_a_effect = 0.1,
+           microbe_a = microbe_a_effect) %>%
+    ungroup()
+
+  # merge both dataframes back together and sort
+  df <- rbind(df_i1, df_i2)
+  df <- df %>% arrange(study_id)
 
   df_i1 <- df %>%
     filter(therapy == "therapy_1") %>%
     group_by(study_id) %>%
     rowwise() %>%
     mutate(therapy_effect = case_when(
-           timepoint == 1 ~ my_sample(0, 0),
+           timepoint == 1 ~ my_sample(1, 1),
            timepoint == 2 ~ my_sample(5, 1),
            timepoint == 3 ~ my_sample(10, 1),
            timepoint == 4 ~ my_sample(20, 1),
-           timepoint == 5 ~ my_sample(40, 1))) %>%
+           timepoint == 5 ~ my_sample(40, 2))) %>%
     ungroup()
 
   df_i2 <- df %>%
@@ -285,47 +317,54 @@ simulated_df_for_workflow <- function(
     group_by(study_id) %>%
     rowwise() %>%
     mutate(therapy_effect = case_when(
-           timepoint == 1 ~ my_sample(0, 0),
-           timepoint == 2 ~ my_sample(4, 1),
-           timepoint == 3 ~ my_sample(8, 1),
-           timepoint == 4 ~ my_sample(12, 1),
-           timepoint == 5 ~ my_sample(16, 1))) %>%
+           timepoint == 1 ~ my_sample(1, 1),
+           timepoint == 2 ~ my_sample(3, 1),
+           timepoint == 3 ~ my_sample(5, 1),
+           timepoint == 4 ~ my_sample(8, 1),
+           timepoint == 5 ~ my_sample(9, 1))) %>%
     ungroup()
 
   # merge both dataframes back together and sort
   df <- rbind(df_i1, df_i2)
   df <- df %>% arrange(study_id)
 
-  df <- df %>%
-    mutate(happiness = happiness_original +
-    rowSums(.[grep("_effect", names(.))]))
-
-  write.table(df, "data/simulation/simulation-effects.txt",
-  row.names = FALSE, sep = "\t")
-  df <- df %>%
-    select(-ends_with("_effect")) %>%
-    select(!(happiness_original))
   return(df)
-  }
+  } # end add_effects function
 
   # remove effect columns and original happiness
   df <- add_effects(df, subjects)
   return(df)
 }
 
-df_a <- simulated_df_for_workflow(happiness_original = rep(30, 5),
-cohort = rep("A", 5),
-total_subjects = 10, study_id_modifier = "a")
+# Make cohort A and B dataframes then merge together
+df_a <- simulated_df_for_workflow(happiness_original =
+rep(happiness_start, total_timepoints),
+cohort = rep("A", total_timepoints),
+total_subjects = subjects_per_cohort, study_id_modifier = "A")
 
-df_b <- simulated_df_for_workflow(happiness_original = rep(30, 5),
-cohort = rep("B", 5),
-total_subjects = 10, study_id_modifier = "b")
-print(df_a)
-print(df_b)
+df_b <- simulated_df_for_workflow(happiness_original =
+rep(happiness_start, total_timepoints),
+cohort = rep("B", total_timepoints),
+total_subjects = subjects_per_cohort, study_id_modifier = "B")
+
 df <- rbind(df_a, df_b)
-print(df)
 
-## Calculate output
+
+# Include effect values in happiness/response
+df <- df %>%
+  mutate(happiness = happiness_original +
+  rowSums(.[grep("_effect", names(.))]))
+
+write.table(df, "data/simulation/simulation-effects.txt",
+row.names = FALSE, sep = "\t")
+
+# Do not keep effect columns in final dataframe
+# the point is to see if vars are selected based on effect
+df <- df %>%
+  select(-ends_with("_effect")) %>%
+  select(!(happiness_original))
+
+# save final dataframe
 write.table(df, out_file, row.names = FALSE, sep = "\t")
 
 
@@ -346,8 +385,7 @@ writeFiles <- function(file_prefix, df, num_features, num_features_prev,
   file_name <- paste0(file_prefix, num_features, "-random-vars.txt")
   write.table(df, file_name, row.names = FALSE, sep = "\t")
 
-  file_name <- paste0(file_prefix, num_features,
-  "-random-vars-shuffled.txt")
+  file_name <- paste0(file_prefix, num_features, "-random-vars-shuffled.txt")
   write.table(df_shuffled_response, file_name, row.names = FALSE, sep = "\t")
 
   return(df)
@@ -362,8 +400,7 @@ addFeatures <- function(df, response_var, file_prefix, low, med, hi) {
   shuffled_response <- sample(df[[response_var]])
 
   # TODO this is repetitive (make list starting with 0)
-  df <- writeFiles(file_prefix, df, low, 0, response_var,
-                   shuffled_response)
+  df <- writeFiles(file_prefix, df, low, 0, response_var, shuffled_response)
   df <- writeFiles(file_prefix, df, med, low, response_var, shuffled_response)
   df <- writeFiles(file_prefix, df, hi, med, response_var, shuffled_response)
 
